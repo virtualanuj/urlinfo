9a0c0e6572cdb2e62a14bb50bba6084e49a8b7bb
Anuj <anujain3@cisco.com>
2018-09-19 07:42:06 +0530

    Unit tests skeleton created

---
+++ tests/test_urlinfo.py
@@ -0,0 +1,18 @@
+import unittest
+
+class Apptests(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testfoo(self):
+        self.assertEqual(1,1)
+
+
+    if __name__ == "__main__":
+        unittest.main()
